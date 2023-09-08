f6c731acd282d1ccbe1337e906586a5917d64e2c
Moayed <136789763+Moayed-Alali@users.noreply.github.com>
2023-09-08 15:50:29 +0300

    Add files via upload
    still working at

---
+++ project.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aq;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Aq {
+
+    public static void main(String[] args) {
+        test t1 = new test();
+        t1.setVisible(true);
+    }
+}
